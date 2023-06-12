ece9f76b60eee1435389dbd91f42fad08fcb6d49
Christian Caron <ccaron@gmail.com>
2023-06-11 21:12:55 -0700

    Cleaned up some comments.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import sys
+from micrograd import Value
+from utils import render
 
 def main() -> int:
+    a = Value(1.0, label='a')
+    c = a + 3.1 + Value(5.5, label='d')
+    render(c)
     return 0
 
 if __name__ == '__main__':
